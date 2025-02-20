fa243d18e6312fe50ab626282d8a3bb57ab13811
raine1124 <kameron.raine24@gmail.com>
2025-02-20 12:52:56 -0800

    Update main.js

--- main.js
+++ main.js
@@ -10,37 +10,31 @@ const handleOnUp = () => {
 const handleOnMove = e => {
   if(track.dataset.mouseDownAt === "0") return;
   
-  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+  const maxDelta = window.innerWidth / 2;
   
-  const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  const percentage = (mouseDelta / maxDelta) * -100;
+  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+  const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
   
   track.dataset.percentage = nextPercentage;
   
+  // Track animation
   track.animate({
     transform: `translate(${nextPercentage}%, -50%)`
   }, { duration: 1200, fill: "forwards" });
   
-  for(const container of track.getElementsByClassName("image-container")) {
-    const image = container.querySelector('.image');
+  // Image animation - this is what creates the parallax effect
+  for(const image of track.getElementsByClassName("image")) {
     image.animate({
-        objectPosition: `${100 + nextPercentage}% center`
+      objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
   }
 }
 
-/* -- Had to add extra lines for touch events -- */
-
 window.onmousedown = e => handleOnDown(e);
-
 window.ontouchstart = e => handleOnDown(e.touches[0]);
-
 window.onmouseup = e => handleOnUp(e);
-
 window.ontouchend = e => handleOnUp(e.touches[0]);
-
 window.onmousemove = e => handleOnMove(e);
-
 window.ontouchmove = e => handleOnMove(e.touches[0]);
